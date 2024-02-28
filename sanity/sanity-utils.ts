b3c093b32da0fbfd4ad3233fea8a8c377d25058f
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-02-28 17:09:29 -0400

    display events on page from

---
+++ sanity/sanity-utils.ts
@@ -0,0 +1,21 @@
+import { createClient, groq } from "next-sanity"
+
+export async function getEvents() {
+    const client = createClient({
+        projectId: "46b4kxer",
+        dataset: "production",
+        apiVersion: "2024-02-27"
+    })
+
+    return client.fetch(
+        groq`*[_type == "event"]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset->url,
+            url,
+            content
+        }`
+    )
+}
