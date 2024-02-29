7eebd0a938b695c77a624df13c63bee52e68fa21
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-02-28 23:39:24 -0400

    updated event schema

--- sanity/schemas/events-schema.ts
+++ sanity/schemas/events-schema.ts
@@ -1,51 +1,62 @@
-
 const event = {
-
     name: 'event',
     title: 'Event',
     type: 'document',
-    fields:[
-        {
-            name: 'name',
-            title: 'name',
+    fields: [
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+        description: 'The name of the event',
+      },
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: { source: 'name' },
+        description: 'The slug for the event URL',
+      },
+      {
+        name: 'date',
+        title: 'Date',
+        type: 'datetime',
+        description: 'The date and time of the event',
+      },
+      {
+        name: 'location',
+        title: 'Location',
+        type: 'string',
+        description: 'The location of the event',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+        options: { hotspot: true },
+        fields: [
+          {
+            name: 'alt',
+            title: 'Alt Text',
             type: 'string',
-        },
-        {
-            name: 'slug',
-            title: 'Slug',
-            type: 'slug',
-            options: {source: 'name'}
-        },
-        {
-            name: 'image',
-            title: 'Image',
-            type: 'image',
-            options: { hotspot: true },
-            fields: [
-                
-                {
-                    name: 'alt',
-                    title: 'Alt',
-                    type: 'string'
-                }
-            ]
-        },
-        {
-            name: 'url',
-            title: 'URL',
-            type: 'url'
-        },
-        {
-            name: 'content',
-            title: 'Content',
-            type: 'array',
-            of: [
-                {
-                    type: 'block'
-                }
-            ]
-        }
-
-    ]
-}
-export default event;
+            description: 'Alternative text for the event image',
+          },
+        ],
+        description: 'Image representing the event',
+      },
+      {
+        name: 'url',
+        title: 'URL',
+        type: 'url',
+        description: 'URL for more information about the event',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'text',
+        description: 'Description of the event',
+      },
+    ],
+  };
+  
+  export default event;
+  
