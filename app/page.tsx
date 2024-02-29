0cf7f23b59c879226958ed9da7f6ec4969bac4fb
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-02-28 20:13:27 -0400

    more visual modification

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,19 @@ export default async function Home() {
   const event = await getEvents();
 
   return (
-    <div> 
+    <div className="max-w-5xl mx-auto py-20"> 
+
+      <h1>Tennis Club Membership</h1>
+
       {event.map((event) => (
-          <div key= {event._id}>{event.name}</div>
+          <div key= {event._id} className="border border-grey-500 rounded-lg">
+            {event.image && (
+              <image src=
+            )}
+          
+          <div className="font-extrabold bg-gradient-to-r">
+            {event.name}
+          </div>
         ))
       }
     </div>
