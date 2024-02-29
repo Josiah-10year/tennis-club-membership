bdb409b95a8946936118d3b7b6c9ebfb7aaab767
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-02-29 08:26:21 -0400

    updated shcema and imported events

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,4 +1,5 @@
 import { createClient, groq } from "next-sanity"
+import { Event } from "../types/Event"
 
 export async function getEvents(): Promise<Event[]> {
     const client = createClient({
@@ -13,9 +14,29 @@ export async function getEvents(): Promise<Event[]> {
             _createdAt,
             name,
             "slug": slug.current,
-            "image": image.asset->url,
-            url,
-            content
+            date,
+            location,
+            discription
         }`
     )
+}
+
+export async function getEvent(slug: string): Promise<Event> {
+    const client = createClient({
+        projectId: "46b4kxer",
+        dataset: "production",
+        apiVersion: "2024-02-27"
+    })
+
+    return client.fetch(
+      groq`*[_type == "event"]{
+          _id,
+          _createdAt,
+          name,
+          "slug": slug.current,
+          date,
+          location,
+          discription
+      }`
+  )
 }
