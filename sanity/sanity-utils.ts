850bc35219dd7cf5bcd30f0b805e984b3a9013a2
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-03-16 17:37:35 -0400

    import and async function

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,5 +1,6 @@
 import { createClient, groq } from "next-sanity"
 import { Event } from "../types/Event"
+import { NoticeBoard } from "../types/NoticeBoard"
 
 export async function getEvents(): Promise<Event[]> {
     const client = createClient({
@@ -39,4 +40,22 @@ export async function getEvent(slug: string): Promise<Event> {
           discription
       }`
   )
+}
+
+export async function getNoticeBoards(): Promise<NoticeBoard[]> {
+    const client = createClient({
+        projectId: "46b4kxer",
+        dataset: "production",
+        apiVersion: "2024-02-27"
+    })
+
+    return client.fetch(
+        groq`*[_type == "noticeBoard"]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            
+        }`
+    )
 }
