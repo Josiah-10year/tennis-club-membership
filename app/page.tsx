8a5478123dd0389b0b0938b4de4bc18c6f351fbc
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-03-20 20:20:02 -0400

    test values for calender page (home page)

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ export default async function Home() {
 
   return (
     <div className="max-w-5xl mx-auto py-20">
-      <h1>Tennis Club Membership</h1>
+      {/* Start here */}
+      <h1>January</h1>
       <div className="mt-5 grid grid-cols-3 gap-8">
         {events.map((event) => (
           <Link 
@@ -33,6 +34,315 @@ export default async function Home() {
             </Link>
         ))}
       </div>
+      {/* from here */}
+      <h1>Feburary</h1>
+      <div className="mt-5 grid grid-cols-3 gap-8">
+        {events.map((event) => (
+          <Link 
+            href="/events/${event.slug}" 
+            key={event._id} 
+            className="border border-grey-500 rounded-lg p-1 hover:scale-105 hover:border-green-500 transition">
+            
+            <div 
+              className="font-extrabold text-xl mb-2 bg-gradient-to-r from-red-500 to-green-500 text-transparent bg-clip-text">
+              {event.name}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Date: {new Date(event.date).toLocaleDateString()} 
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Time: {new Date(event.date).toLocaleTimeString()}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Location: {event.location}
+            </div>
+            </Link>
+        ))}
+      </div>
+      {/* from here */}
+      <h1>March</h1>
+      <div className="mt-5 grid grid-cols-3 gap-8">
+        {events.map((event) => (
+          <Link 
+            href="/events/${event.slug}" 
+            key={event._id} 
+            className="border border-grey-500 rounded-lg p-1 hover:scale-105 hover:border-green-500 transition">
+            
+            <div 
+              className="font-extrabold text-xl mb-2 bg-gradient-to-r from-red-500 to-green-500 text-transparent bg-clip-text">
+              {event.name}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Date: {new Date(event.date).toLocaleDateString()} 
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Time: {new Date(event.date).toLocaleTimeString()}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Location: {event.location}
+            </div>
+            </Link>
+        ))}
+      </div>
+      {/* from here */}
+      <h1>April</h1>
+      <div className="mt-5 grid grid-cols-3 gap-8">
+        {events.map((event) => (
+          <Link 
+            href="/events/${event.slug}" 
+            key={event._id} 
+            className="border border-grey-500 rounded-lg p-1 hover:scale-105 hover:border-green-500 transition">
+            
+            <div 
+              className="font-extrabold text-xl mb-2 bg-gradient-to-r from-red-500 to-green-500 text-transparent bg-clip-text">
+              {event.name}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Date: {new Date(event.date).toLocaleDateString()} 
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Time: {new Date(event.date).toLocaleTimeString()}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Location: {event.location}
+            </div>
+            </Link>
+        ))}
+      </div>
+      {/* from here */}
+      <h1>May</h1>
+      <div className="mt-5 grid grid-cols-3 gap-8">
+        {events.map((event) => (
+          <Link 
+            href="/events/${event.slug}" 
+            key={event._id} 
+            className="border border-grey-500 rounded-lg p-1 hover:scale-105 hover:border-green-500 transition">
+            
+            <div 
+              className="font-extrabold text-xl mb-2 bg-gradient-to-r from-red-500 to-green-500 text-transparent bg-clip-text">
+              {event.name}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Date: {new Date(event.date).toLocaleDateString()} 
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Time: {new Date(event.date).toLocaleTimeString()}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Location: {event.location}
+            </div>
+            </Link>
+        ))}
+      </div>
+      {/* from here */}
+      <h1>June</h1>
+      <div className="mt-5 grid grid-cols-3 gap-8">
+        {events.map((event) => (
+          <Link 
+            href="/events/${event.slug}" 
+            key={event._id} 
+            className="border border-grey-500 rounded-lg p-1 hover:scale-105 hover:border-green-500 transition">
+            
+            <div 
+              className="font-extrabold text-xl mb-2 bg-gradient-to-r from-red-500 to-green-500 text-transparent bg-clip-text">
+              {event.name}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Date: {new Date(event.date).toLocaleDateString()} 
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Time: {new Date(event.date).toLocaleTimeString()}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Location: {event.location}
+            </div>
+            </Link>
+        ))}
+      </div>
+      {/* from here */}
+      <h1>July</h1>
+      <div className="mt-5 grid grid-cols-3 gap-8">
+        {events.map((event) => (
+          <Link 
+            href="/events/${event.slug}" 
+            key={event._id} 
+            className="border border-grey-500 rounded-lg p-1 hover:scale-105 hover:border-green-500 transition">
+            
+            <div 
+              className="font-extrabold text-xl mb-2 bg-gradient-to-r from-red-500 to-green-500 text-transparent bg-clip-text">
+              {event.name}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Date: {new Date(event.date).toLocaleDateString()} 
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Time: {new Date(event.date).toLocaleTimeString()}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Location: {event.location}
+            </div>
+            </Link>
+        ))}
+      </div>
+      {/* from here */}
+      <h1>August</h1>
+      <div className="mt-5 grid grid-cols-3 gap-8">
+        {events.map((event) => (
+          <Link 
+            href="/events/${event.slug}" 
+            key={event._id} 
+            className="border border-grey-500 rounded-lg p-1 hover:scale-105 hover:border-green-500 transition">
+            
+            <div 
+              className="font-extrabold text-xl mb-2 bg-gradient-to-r from-red-500 to-green-500 text-transparent bg-clip-text">
+              {event.name}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Date: {new Date(event.date).toLocaleDateString()} 
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Time: {new Date(event.date).toLocaleTimeString()}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Location: {event.location}
+            </div>
+            </Link>
+        ))}
+      </div>
+      {/* from here */}
+      <h1>September</h1>
+      <div className="mt-5 grid grid-cols-3 gap-8">
+        {events.map((event) => (
+          <Link 
+            href="/events/${event.slug}" 
+            key={event._id} 
+            className="border border-grey-500 rounded-lg p-1 hover:scale-105 hover:border-green-500 transition">
+            
+            <div 
+              className="font-extrabold text-xl mb-2 bg-gradient-to-r from-red-500 to-green-500 text-transparent bg-clip-text">
+              {event.name}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Date: {new Date(event.date).toLocaleDateString()} 
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Time: {new Date(event.date).toLocaleTimeString()}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Location: {event.location}
+            </div>
+            </Link>
+        ))}
+      </div>
+      {/* from here */}
+      <h1>October</h1>
+      <div className="mt-5 grid grid-cols-3 gap-8">
+        {events.map((event) => (
+          <Link 
+            href="/events/${event.slug}" 
+            key={event._id} 
+            className="border border-grey-500 rounded-lg p-1 hover:scale-105 hover:border-green-500 transition">
+            
+            <div 
+              className="font-extrabold text-xl mb-2 bg-gradient-to-r from-red-500 to-green-500 text-transparent bg-clip-text">
+              {event.name}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Date: {new Date(event.date).toLocaleDateString()} 
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Time: {new Date(event.date).toLocaleTimeString()}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Location: {event.location}
+            </div>
+            </Link>
+        ))}
+      </div>
+      {/* from here */}
+      <h1>November</h1>
+      <div className="mt-5 grid grid-cols-3 gap-8">
+        {events.map((event) => (
+          <Link 
+            href="/events/${event.slug}" 
+            key={event._id} 
+            className="border border-grey-500 rounded-lg p-1 hover:scale-105 hover:border-green-500 transition">
+            
+            <div 
+              className="font-extrabold text-xl mb-2 bg-gradient-to-r from-red-500 to-green-500 text-transparent bg-clip-text">
+              {event.name}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Date: {new Date(event.date).toLocaleDateString()} 
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Time: {new Date(event.date).toLocaleTimeString()}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Location: {event.location}
+            </div>
+            </Link>
+        ))}
+      </div>
+      {/* from here */}
+      <h1>December</h1>
+      <div className="mt-5 grid grid-cols-3 gap-8">
+        {events.map((event) => (
+          <Link 
+            href="/events/${event.slug}" 
+            key={event._id} 
+            className="border border-grey-500 rounded-lg p-1 hover:scale-105 hover:border-green-500 transition">
+            
+            <div 
+              className="font-extrabold text-xl mb-2 bg-gradient-to-r from-red-500 to-green-500 text-transparent bg-clip-text">
+              {event.name}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Date: {new Date(event.date).toLocaleDateString()} 
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Time: {new Date(event.date).toLocaleTimeString()}
+            </div>
+            <div 
+              className="text-gray-600 mb-2">
+              Location: {event.location}
+            </div>
+            </Link>
+        ))}
+      </div>
+      {/* from here */}
     </div>
-  );
+   );
 }
