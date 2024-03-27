7c175dd9538941ac9fa7c5ae8e097f106751017d
joshuamvw <joshmalvalwil1@gmail.com>
2024-03-27 13:54:24 -0400

    test to see if my changes are made now?

--- app/court_bookings/page.tsx
+++ app/court_bookings/page.tsx
@@ -4,6 +4,7 @@ export default async function CourtBookings(){
             <h3>Court Bookings Page</h3>
             <h3>The calendar and booking details</h3>
             <h3>The content is being hidden by the nav bar but its here</h3>
+            <p>Testing testing 123</p>
         </div>
 
     );
