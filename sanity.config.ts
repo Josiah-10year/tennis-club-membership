e1862cead8348ea09febdd2e1610fa8cdf68722e
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-04-08 06:42:58 -0400

    identification of import statements

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
+//events
 import event from "./sanity/schemas/events-schema";
+
+//court imports
 import court from "./sanity/schemas/courts-schema";
 import courtBooking from "./sanity/schemas/court-booking-schema";
+
+//user imports
 import user from "./sanity/schemas/user-schema";
+//?? imports
 import topic from "./sanity/schemas/topic-schema";
 import interest from "./sanity/schemas/interest-schema";
+
+//post boards imports
 import comment from "./sanity/schemas/comment-schema";
 import post from "./sanity/schemas/post-schema";
 
