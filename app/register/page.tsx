df8ff37a4e937e3e7df431a828a16ba9d3a1eac4
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-04-08 16:33:07 -0400

    test resolve

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import { getAllTopics, getAllInterests, getAllUsers } from "../../sanity/sanity-utils";
-=======
 import { getAllTopics, getAllInterests, getAllUsersUsernameAndEmail } from "../../sanity/sanity-utils";
 import { useState } from "react";
->>>>>>> 7345bf8fb2bfa30bf9e494b8935eeb72c3ae2d4a
 import { SubmitHandler, useForm } from "react-hook-form";
 import Form from "../components/RegisterSH"
 import { URL } from "url";
