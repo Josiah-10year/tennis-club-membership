46606f2594913de04ca22005470682d4ada8f5d4
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-04-08 13:55:14 -0400

    removed comments

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,31 +1,4 @@
-// import { getEvent } from "@/sanity/sanity-utils";
-
-// type Props = {
-//     params: { event: string }
-// }
-
-// export default async function Events({params}: Props) {
-
-//     const slug = params.event;
-//     const event = await getEvent(slug);
-
-//     if (!event) {
-//         return <div>No event found</div>;
-//     } 
-//     //to be turned into a 404 error handeling
-
-//     return(
-//         <div>
-//             <h3>Event Details</h3>
-//             <h3>The schdeule of events will go here</h3>
-//             <h3>The content is being hidden by the nav bar but its here</h3>
-//             <div>{event.name}</div>
-//         </div>
-
-//     );
-
-// }
-
+"use client"
 import { getEvents } from "../../sanity/sanity-utils";
 import Link from "next/link";
 
