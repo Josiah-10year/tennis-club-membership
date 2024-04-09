3c13a0d2ded21766a0a665ab3160d574f1980f44
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-04-09 11:18:13 -0400

    reate naviga

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -10,12 +10,13 @@ export default async function Posts() {
         <div className="max-w-5xl mx-auto py-20">
             <div className="mt-5 grid grid-cols-3 gap-8">
                 {posts.map((post) => (
-                    <div key={post._id} className="border p-4 rounded">
+                    <Link href={`/posts/${post.slug}`} 
+                    key={post._id} className="border p-4 rounded hover:scale-105 hover:border-green-500 transition">
                         <h2 className="text-xl font-bold">{post.title}</h2>
                         <p className="text-gray-500">{post.description}</p>
                         <p className="text-sm text-gray-400 mt-2">Author: {post.author}</p>
                         {/* You can display images here if needed */}
-                    </div>
+                    </Link>
                 ))}
             </div>
         </div>
