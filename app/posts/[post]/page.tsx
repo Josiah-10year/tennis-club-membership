3b041fb0b2346e45c787777b6758bb21ed181077
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-04-10 23:46:55 -0400

    adding comments

--- app/posts/[post]/page.tsx
+++ app/posts/[post]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { getPost } from "../../../sanity/sanity-utils";
-
+import { getComments } from "../../../sanity/sanity-utils";
 type Props={
     params: { post: string }
 }
@@ -8,6 +8,7 @@ type Props={
 export default async function Project({ params }: Props) {
     const slug = params.post;
     const post = await getPost(slug);
+    const comments = await getComments();
     console.log(post)
 
     if (!post) {
@@ -17,6 +18,7 @@ export default async function Project({ params }: Props) {
         <div>
             <h1>{post.title}</h1>
             <p className="text-gray-500">{post.description}</p>
+            {/* <p>{comments.text}</p> */}
         </div>
     );
 
