a50fe96d4e3239769136e46a6da411d57b66dcf1
shanif <97924668+salamandur@users.noreply.github.com>
2024-04-14 20:28:25 +0000

    upload club logo

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -12,8 +12,8 @@ const Header = () => (
           target="_self"
           className="flex flex-col justify-between items-center min-w-[300px] ml-3"
         >
-          <div className="font-lulo mb-3">ST. AUGUSTINE RECREATIONAL CLUB</div>
-          <div className="text-xs tracking-widest">Slogan</div>
+          <img className="w-8 h-8 rounded-full mr-2" src="https://github.com/Josiah-10year/tennis-club-membership/blob/21819b412dec202fe45d8a1bacb6be58b7cf73eb/app/images/club-logo.png?raw=true" ></img>
+          <div className="font-bold sm:text-sm lg:text-xl xl:text-xl mb-3">St. Augustine Recreational Club</div>
         </a>
         <div className="flex-grow pb-5 pr-5">
           <NavBar />
