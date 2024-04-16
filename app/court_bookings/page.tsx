4a9bd298e9d52c01687aabdaccff0be5702c022c
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-04-15 20:15:31 -0400

    doc formatting

--- app/court_bookings/page.tsx
+++ app/court_bookings/page.tsx
@@ -1,41 +1,31 @@
 import { add, format } from "date-fns";
 import { INTERVAL, STORE_CLOSING_TIME, STORE_OPENING_TIME } from '../constants/config'
-import { getCourtBookings, getCourtBookingsAfterToday, getCourts, getUser } from "../../sanity/sanity-utils";
+import { getCourtBookingsAfterToday, getCourts, getUser } from "../../sanity/sanity-utils";
 import Calendar from "../components/CalendarTestJV"
 import { CourtBooking } from "@/types/CourtBooking";
 import { authConfig, useloginIsRequiredServer } from "@/app/lib/auth";
 import { getServerSession } from "next-auth";
 import { Court } from "@/types/Court";
 
-export default async function CourtBookings(){
+export default async function CourtBookings() {
     await useloginIsRequiredServer();
 
     const courtsArray = await getCourts();
-    //organize array of days that are booked up, and also pass array of all bookings >= today; array of filled days used for calendar, and
-    //array of all bookings >= today used to check the time slots given court ID
-
-    const courtBookingsArray : CourtBooking[] = await getCourtBookingsAfterToday();
-
-    // const today = new Date();
-    // today.setHours(0, 0, 0, 0);
-    // const url = 'https://46b4kxer.api.sanity.io/v2021-06-07/data/query/production?query=*[_type =="booking"][start >= "' + today.toISOString() + '"]'
-    // // const testArray1 : CourtBooking[] = await fetchData(url);
-    // //const testArray : CourtBooking[] = testArray1.
-    // console.log("Test: " + testArray1)
+    const courtBookingsArray: CourtBooking[] = await getCourtBookingsAfterToday();
 
     const getFullyBookedDates = (courtBookings: CourtBooking[]) => {
         const fullyBookedDatesSet = new Set<string>();
-    
+
         courtBookings.forEach(booking => {
             const bookingStartDate = new Date(booking.start);
             const bookingDateISO = format(bookingStartDate, "yyyy-MM-dd");
-            
+
             // Check if all time slots for the day are booked
             let allTimeSlotsBooked = true;
             const beginning = add(new Date(bookingDateISO), { hours: STORE_OPENING_TIME });
             const end = add(new Date(bookingDateISO), { hours: STORE_CLOSING_TIME });
             const interval = INTERVAL;
-    
+
             for (let i = beginning; i <= end; i = add(i, { minutes: interval })) {
                 const timeSlot = format(i, "yyyy-MM-dd'T'HH:mm:ssxxx");
                 let timeSlotBooked = false;
@@ -51,43 +41,40 @@ export default async function CourtBookings(){
                     break;
                 }
             }
-    
+
             // If all time slots for the day are booked, add the date to the set
             if (allTimeSlotsBooked) {
                 fullyBookedDatesSet.add(bookingDateISO);
             }
         });
-    
+
         return Array.from(fullyBookedDatesSet);
     }
-    
+
     const fullyBookedDates = getFullyBookedDates(courtBookingsArray);
 
-    
     function sortCourtsByName(courts: Court[]): Court[] {
 
         return courts.slice().sort((a, b) => a.name.localeCompare(b.name));
     }
-        const courts = sortCourtsByName(courtsArray);
+    const courts = sortCourtsByName(courtsArray);
 
-        //we also need to pull the user id but for now
-        const session = await getServerSession(authConfig);
-        const username = session?.user?.email
-        let userID = ""
-        if (username){
-            const userID2 = await getUser(username);
-            userID = userID2[0]._id
-        }else{
+    //we also need to pull the user id but for now
+    const session = await getServerSession(authConfig);
+    const username = session?.user?.email
+    let userID = ""
+    if (username) {
+        const userID2 = await getUser(username);
+        userID = userID2[0]._id
+    } else {
         //something wrong defaulted to john
         userID = "68028cac-7cde-4489-be2c-a601df250af0"
-        }
+    }
 
-
-
-    return(
+    return (
         <div className="max-w-5xl mx-auto py-20">
             <h1 className="text-left py-8">Court Booking</h1>
-            <Calendar courtArrayProp={courts} fullyBookedDates={fullyBookedDates} courtBookingsArray={courtBookingsArray} userID= {userID}/>
+            <Calendar courtArrayProp={courts} fullyBookedDates={fullyBookedDates} courtBookingsArray={courtBookingsArray} userID={userID} />
         </div>
     );
 }
