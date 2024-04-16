20247abe1eb4c072f9b3d12f5a319938661fd611
Josiah <jojotenia@gmail.com>
2024-04-16 10:28:26 -0400

    added needed padding

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -14,7 +14,7 @@ export default async function Posts() {
         <div className="max-w-5xl mx-auto py-20">
             <h1 className="text-left py-8">Posts</h1>
             <div className="grid space-y-12 md:gap-8 lg:grid-cols-12 lg:gap-16 lg:space-y-0 xl:gap-16">
-                <div className="lg:col-span-4 xl:col-span-3">
+                <div className="lg:col-span-4 xl:col-span-3 p-4"> {/* Added padding here */}
                     <div className="lg:block">
                         <h1 className="text-xl">Categories</h1>
                         <ul>
@@ -23,13 +23,13 @@ export default async function Posts() {
                             </li>
                             {topics.map((topic, index) => (
                                 <li key={index}> {/* Use index as key for topics */}
-                                    <a  key={index+1} href={`/posts/topic/${topic.name}`} className="text-lime-800 hover:text-lime-600 transition text-foreground-light block text-base">{topic.name}</a>
+                                    <a key={index+1} href={`/posts/topic/${topic.name}`} className="text-lime-800 hover:text-lime-600 transition text-foreground-light block text-base">{topic.name}</a>
                                 </li>
                             ))}
                         </ul>
                     </div>
                 </div>
-                <div className="lg:col-span-8 xl:col-span-9">
+                <div className="lg:col-span-8 xl:col-span-9 p-4"> {/* Added padding here */}
                     <h1 className="text-2xl">All Posts</h1>
                     <div className="grid grid-cols-1 gap-5 lg:max-w-none lg:grid-cols-2 xl:grid-cols-3">
                         {posts.map((post) => (
