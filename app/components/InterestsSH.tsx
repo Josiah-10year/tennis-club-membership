f1a04b08ea7a01c03a86c78027ab6d8f94d36ec1
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-04-15 20:12:48 -0400

    doc formatting

--- app/components/InterestsSH.tsx
+++ app/components/InterestsSH.tsx
@@ -9,18 +9,18 @@ interface indexProps {
     interestsArrayProp: Interest[];
 }
 
-const index: FC<indexProps> =  ({ interestsArrayProp }) => {
+const index: FC<indexProps> = ({ interestsArrayProp }) => {
     const interests = interestsArrayProp;
 
     return (
         <div>
             <label className="font-semibold text-gray-800 text-sm" htmlFor="interests">
-                    Interests
+                Interests
             </label>
             <div className="flex flex-col sm:flex-row sm:gap-12">
                 {interests?.map((interest, i) => (
                     <div key={`topic._id-${i}`}>
-                        <input className="text-xs interest" value={interest._id} type="checkbox" key={`topic._id-${i}`}/>
+                        <input className="text-xs interest" value={interest._id} type="checkbox" key={`topic._id-${i}`} />
                         <label className="text-xs" htmlFor={`topic._id-${i}`}> {interest.name}</label>
                     </div>
                 ))}
