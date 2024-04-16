03e0a5b16313b3069546eafbdd44b107f9b6956b
Josiah <jojotenia@gmail.com>
2024-04-15 22:41:36 -0400

    fixed warning

--- app/page.tsx
+++ app/page.tsx
@@ -20,32 +20,20 @@ export default async function Home() {
     <div className="max-w-5xl mx-auto justify-center py-20">
       <div className="max-w-5xl flex flex-col container justify-center items-center mx-auto py-20 bg-cover" style={{ backgroundImage: `url('https://raw.githubusercontent.com/Josiah-10year/tennis-club-membership/master/app/images/tennis-club.jpg')` }}>
         <h1 className="text-white text-center py-8">St. Augustine Recreational Club</h1>
-        {session? (
-          loggedInUser? (
-            loggedInUser.role === "admin" ? (
-              <div>
-                <a
-                href="/admin/structure"
-              >
-                <p className="bg-gray-900 p-1 border-white border text-white bg-opacity-50 text-center rounded-lg hover:bg-opacity-100 hover:scale-105">Admin View</p>
-              </a>
-              </div>
-            ) : (
-              <div></div>
-            )
-          ):(
-            <div></div>
-          )
-        ):(
+        {session && loggedInUser && loggedInUser.role === "admin" && (
           <div>
-            <a
-              href="/login"
-            >
+            <a href="/admin/structure">
+              <p className="bg-gray-900 p-1 border-white border text-white bg-opacity-50 text-center rounded-lg hover:bg-opacity-100 hover:scale-105">Admin View</p>
+            </a>
+          </div>
+        )}
+        {!session && (
+          <div>
+            <a href="/login">
               <p className="bg-gray-900 p-1 border-white border text-white bg-opacity-50 text-center rounded-lg hover:bg-opacity-100 hover:scale-105">Member Login/Register</p>
             </a>
           </div>
         )}
-        
       </div>
       <div className="max-w-5xl flex flex-col container justify-center items-center mx-auto py-4">
         <p className="text-center">This is a recreational club which naturally beckons with its excellent location and easy access to all. Be it an intense workout on one of the four tennis courts or a relaxing evening at the restaurant.</p>
