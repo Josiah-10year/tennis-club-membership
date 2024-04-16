a85124728e6262653f26d9b09bb7af7f08d83b35
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-04-15 20:17:28 -0400

    removal of unused imports

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,5 @@
 import { getAllTopics, getAllInterests, getAllUsersUsernameAndEmail } from "../../sanity/sanity-utils";
-import { useState } from "react";
 import Form from "../components/RegisterSH"
-import { URL } from "url";
 
 export default async function Register(){
   const topics = await getAllTopics();
