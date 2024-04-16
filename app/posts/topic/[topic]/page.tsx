7eba681a44f1f869894e5d60470a158aa5408174
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-04-15 20:17:20 -0400

    removal of unused imports and comments

--- app/posts/topic/[topic]/page.tsx
+++ app/posts/topic/[topic]/page.tsx
@@ -1,35 +1,13 @@
 "use client"
-import { use } from "react";
 import { getAllTopics, getPostsByTopic, getTopicbyName } from "../../../../sanity/sanity-utils";
 import Link from "next/link";
-import { useRouter } from 'next/router';
-import { FaArrowCircleUp } from 'react-icons/fa';
-import { authConfig, loginIsRequiredClient } from "@/app/lib/auth";
-import { getServerSession } from "next-auth";
 import BackToTopButton from "@/app/components/BackToTopButton";
-
-
 
 type Props = {
     params: { topic: string }
 }
 
 export default async function EventDetails({ params }: Props) {
-
-    // const session = await getServerSession(authConfig);
-
-    // let username: string | null | undefined = null
-    // if(session){
-    //     username = session?.user?.email
-    //     if(typeof username == "undefined" || !username)
-    //         username = "-1"
-    // }
-
-    // console.log(username)
-
-    // const users = await getUser(username);
-    // user = users[0]
-
     //properly format info
     const formattedTopic = decodeURIComponent(params.topic)
 
@@ -65,19 +43,19 @@ export default async function EventDetails({ params }: Props) {
                 </div>
                 <div className="lg:col-span-8 xl:col-span-9">
                     <h1 className="text-2xl">{formattedTopic}</h1>
-                        {posts[0] ? (
-                            <div className="grid grid-cols-1 gap-5 lg:max-w-none lg:grid-cols-2 xl:grid-cols-3">
-                                {posts?.map((post) => (
-                                    <Link href={`/posts/${post.slug}`} 
+                    {posts[0] ? (
+                        <div className="grid grid-cols-1 gap-5 lg:max-w-none lg:grid-cols-2 xl:grid-cols-3">
+                            {posts?.map((post) => (
+                                <Link href={`/posts/${post.slug}`}
                                     key={post._id} className="border p-4 rounded hover:scale-105 hover:border-blue-500 transition">
-                                        <h2 className="text-xl font-semibold text-gray-800">{post.title}</h2>
-                                        <p className="text-sm text-gray-600">{post.description}</p>
-                                        {/* <p className="text-sm text-gray-400 mt-2">Author: {post.author}</p> */}
-                                        {/* You can display images here if needed */}
-                                    </Link>
-                                ))}
-                            </div>
-                        ) :
+                                    <h2 className="text-xl font-semibold text-gray-800">{post.title}</h2>
+                                    <p className="text-sm text-gray-600">{post.description}</p>
+                                    {/* <p className="text-sm text-gray-400 mt-2">Author: {post.author}</p> */}
+                                    {/* You can display images here if needed */}
+                                </Link>
+                            ))}
+                        </div>
+                    ) :
                         (
                             <div className="grid grid-cols-1 gap-5 lg:max-w-none lg:grid-cols-2 xl:grid-cols-3">
                                 <p className="text-sm text-gray-600 p-4">No posts under this category as yet</p>
@@ -85,7 +63,6 @@ export default async function EventDetails({ params }: Props) {
                         )}
                 </div>
             </div>
-            {/* Back to top button */}
             <BackToTopButton />
         </div>
     );
