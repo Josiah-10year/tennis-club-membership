3c303038f443e8c63cb76584c789a0a9d2417956
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-04-15 20:25:04 -0400

    made footer stay at page base

--- app/components/Layout/Footer.tsx
+++ app/components/Layout/Footer.tsx
@@ -1,9 +1,10 @@
 import './footer.css';
 
 const Footer = () => (
-  <div className="text-sm py-4 flex justify-center bg-lime-100">
+  <div className="footer text-sm py-4 flex justify-center bg-lime-100">
     <p className='text-lime-900'>©2024 St. Augustine Recreational Club</p>
   </div>
 );
 
 export default Footer;
+
