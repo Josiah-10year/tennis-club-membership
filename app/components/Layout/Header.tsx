1dd8aeb064855f236b28fd2049e9c473f52695ee
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-04-15 20:14:12 -0400

    doc formatting

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -4,16 +4,15 @@ const Header = () => (
   <>
     <header
       className="absolute md:fixed bg-lime-100 w-full"
-      
-    //   data-testid={testIds.LAYOUT.HEADER}
+
     >
-      
+
       <div className="relative flex justify-center py-3 max-w-full-content mx-auto gap-8 h-header items-center">
         <a
           href="/"
           target="_self"
           className="w-auto pl-2"
-          >
+        >
           <div className="flex justify-start items-center">
             <img className="w-auto h-12 rounded mr-2" src="https://raw.githubusercontent.com/Josiah-10year/tennis-club-membership/new-opps/app/images/club-logo.png" ></img>
             <p className="text-lime-900 font-semibold text-base md:hidden">St. Augustine Recreational Club</p>
